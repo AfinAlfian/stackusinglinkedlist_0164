f07491917654e8fb94adaee854f8e0f23e29e40e
AfinAlfian <alfinalfiansyah528@gmail.com>
2024-05-11 12:09:47 +0700

    prosedur untuk melihat data teratas

--- StackLinkedList.cpp
+++ StackLinkedList.cpp
@@ -50,5 +50,14 @@ public:
 		{
 			cout << "List is empty." << endl;
 		}
+		else {
+			Node* current = top;
+			while (current != NULL)
+			{
+				cout << current->data << " " << endl;
+				current = current->next;
+			}
+			cout << endl;
+		}
 	}
 };
