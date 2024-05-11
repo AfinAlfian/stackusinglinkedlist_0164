ba4632899211a7b5e2c4d588c5b8add0778f02fe
AfinAlfian <alfinalfiansyah528@gmail.com>
2024-05-11 12:16:39 +0700

    pilihan 1 untuk menjalankan prosedur memasukkan nilai

--- StackLinkedList.cpp
+++ StackLinkedList.cpp
@@ -86,6 +86,11 @@ int main()
 
 		switch (choice)
 		{
+		case 1:
+			cout << "Enter the value to push: ";
+			cin >> value;
+			stack.push(value);
+			break;
 		default:
 			cout << "Invaid choice. Try again." << endl;
 			break;
