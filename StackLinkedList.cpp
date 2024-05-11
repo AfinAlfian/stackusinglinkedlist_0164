e299b181b79e51eab87bba7fb879ab3387119907
AfinAlfian <alfinalfiansyah528@gmail.com>
2024-05-11 12:18:40 +0700

    pilihan 4 untuk keluar dari program

--- StackLinkedList.cpp
+++ StackLinkedList.cpp
@@ -106,6 +106,8 @@ int main()
 				cout << "Stack is empty. No top value" << endl;
 			}
 			break;
+		case 4:
+			cout << "Exiting program." << endl;
 			break;
 		default:
 			cout << "Invaid choice. Try again." << endl;
